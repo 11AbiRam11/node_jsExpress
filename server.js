9e149163fe4418be5d82d56c33218cbafd6b8330
11AbiRam11 <abhi555rram@gmail.com>
2025-02-21 17:30:10 +0530

    changed few lines in html files

--- server.js
+++ server.js
@@ -29,7 +29,7 @@ app.get('/testing', (req, res) => {
 
 app.get('/client_res', (req, res) => {
     res.sendFile(path.join(__dirname,'public','index.html'))
-    server.emit('client_status','Server is online')
+    // server.emit('client_status','Server is online')
 })
 
 
